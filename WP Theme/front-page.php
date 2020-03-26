092ca69c498440fd0fdfc4b754690d24d49ebe25
chansestephens <53543292+chansestephens@users.noreply.github.com>
2020-03-25 22:22:56 -0500

    Updates to front-page, header, style.css

---
+++ WP Theme/front-page.php
@@ -0,0 +1,44 @@
+<?php  get_header();?>
+
+<!-- Full Page Intro Chanse S 3/25 -->
+<div class="view full-page-intro front-page-img background-tint">
+
+    <!-- Mask & flexbox options-->
+    <div class="d-flex justify-content-center align-items-center">
+
+        <!-- Content -->
+        <div class="container front-page-hero">
+            <!--Grid row-->
+            <div class="row wow fadeIn">
+                <!--Grid column-->
+                <div class="col-md-12 mb-4 white-text text-center">
+                    <h1 class="display-3 font-weight-bold">The Talk Shouldn't be Scary</h1>
+                    <hr class="hr-light">
+                    <p class="mb-4 d-none d-md-block">
+                        <h2>Join The Talk Made Simple to learn, discover, and understand.</h2>
+                    </p>
+                    <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/" class="btn btn-indigo btn-lg mt-4">Get Started for Free</a>
+                </div>
+                <!--Grid column-->
+
+            
+            </div>
+            <!--Grid row-->
+        </div>
+        <!-- Content -->
+    </div>
+    <!-- Mask & flexbox options-->
+</div>
+<!-- Full Page Intro Chanse S 3/25 -->
+
+<!--Main layout-->
+    <main>
+        <div class="container">
+
+        
+
+        </div>
+    </main>
+    <!--Main layout-->
+
+<?php  get_footer(); ?>
