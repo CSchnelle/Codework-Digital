fd15f9e98fbf617bc1411d90aace8e1228cc5e71
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-04-03 10:22:42 -0500

    Update footer.php
    Edited Widgets

--- WP Theme/footer.php
+++ WP Theme/footer.php
@@ -35,25 +35,6 @@ $("#mdb-navigation > ul > li > a").addClass("page-link")
             <i class="fa fa-youtube mr-3"></i>
         </a>
 
-        <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
-            <i class="fab fa-google-plus-g mr-3"></i>
-        </a>
-
-        <a href="https://dribbble.com/mdbootstrap" target="_blank">
-            <i class="fa fa-dribbble mr-3"></i>
-        </a>
-
-        <a href="https://pinterest.com/mdbootstrap" target="_blank">
-            <i class="fa fa-pinterest mr-3"></i>
-        </a>
-
-        <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
-            <i class="fa fa-github mr-3"></i>
-        </a>
-
-        <a href="http://codepen.io/mdbootstrap/" target="_blank">
-            <i class="fa fa-codepen mr-3"></i>
-        </a>
     </div>
     <!-- Social icons -->
 
